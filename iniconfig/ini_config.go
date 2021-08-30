e7ac5ce175ad4a0a6f7e87871cf5948a77bedcc6
joeynkay <cyou1029122023@gmail.com>
2021-08-30 23:48:30 +0800

    1.[add]开发UnMarshal函数，还欠缺字段的解析

--- iniconfig/ini_config.go
+++ iniconfig/ini_config.go
@@ -13,10 +13,9 @@ func UnMarshal(data []byte, result interface{}) (err error) {
 	// 获取文本行数组
 	lineArr := strings.Split(string(data), "\n")
 
-	fmt.Println(lineArr)
+	// fmt.Println(lineArr)
 	// 获取result的反射类型对象
 	ptrTypeObj := reflect.TypeOf(result)
-	fmt.Println(ptrTypeObj)
 	// 判断种类是否为指针
 	if ptrTypeObj.Kind() != reflect.Ptr {
 		err = fmt.Errorf("please pass address")
@@ -33,6 +32,7 @@ func UnMarshal(data []byte, result interface{}) (err error) {
 	// 开始按行处理文本内容
 	for index, line := range lineArr {
 		line = strings.TrimSpace(line)
+		// fmt.Println(line)
 		// 忽略空行
 		if len(line) == 0 {
 			continue
@@ -44,21 +44,80 @@ func UnMarshal(data []byte, result interface{}) (err error) {
 		// 判断开头首字符是否为'['
 		// 如果是节点名称行
 		if line[0] == '[' {
-			lastSectionName = parseSectionName(line)
+			lastSectionName, err = parseSectionName(line, elemTypeObj)
+			// fmt.Println(lastSectionName)
+			if err != nil {
+				err = fmt.Errorf("%v, lineno:%d", err, index+1)
+				return
+			}
 			continue
 		}
 		// 如果是节点字段行
-		parseItemName(line, lastSectionName, result)
+		// err = parseItemName(line, lastSectionName, result)
 
 	}
+	_ = lastSectionName
 	return
 }
 
-// 解析节点名称行，返回节点名称
-func parseSectionName(line string, result interface{}) (lastSectionName string, err error) {
+// 解析字段名称
+func parseItemName(line string, sectionName string, result interface{}) (err error) {
+	index := strings.Index(line, "=")
+	// 判断是否有等号
+	if index == -1 {
+		err = fmt.Errorf("syntax error, line:%s", line)
+		return
+	}
+
+	// 获取左边的key值
+	key := strings.TrimSpace(line[0:index])
+	// 获取右边的value值
+	value := strings.TrimSpace(line[index+1:])
+
+	_ = value
+	// 判断key值的长度
+	if len(key) == 0 {
+		err = fmt.Errorf("syntax error, line:%s", line)
+		return
+	}
+
+	// 获取节点的类型对象
+	valueObj := reflect.ValueOf(result)
+	sectionValue := valueObj.Elem().FieldByName(sectionName)
+	sectionType := sectionValue.Type()
+
+	if sectionType.Kind() != reflect.Struct {
+		err = fmt.Errorf("%s must be struct", sectionName)
+		return
+	}
+
+	return
+}
+
+// 解析节点名称，返回节点名称
+func parseSectionName(line string, TypeObj reflect.Type) (fieldName string, err error) {
 	// 判断节点格式是否正确
 	if line[len(line)-1] != ']' {
-		err = fmt.Errorf("syntax error, line:%s", line)
+		err = fmt.Errorf("syntax error, invalid section:%s", line)
+		return
+	}
+	if line[0] == '[' && line[len(line)-1] == ']' {
+		// 获取中括号内的内容
+		sectionName := strings.TrimSpace(line[1 : len(line)-1])
+		// 判断中括号内的内容是否为空
+		if len(sectionName) == 0 {
+			err = fmt.Errorf("syntax error, invalid section:%s", line)
+			return
+		}
+		// 遍历类型对象，获取其字段名称
+		for i := 0; i < TypeObj.NumField(); i++ {
+			field := TypeObj.Field(i)
+			tagValue := field.Tag.Get("ini")
+			if tagValue == sectionName {
+				fieldName = field.Name
+				break
+			}
+		}
 	}
 	return
 }
--- iniconfig/ini_config_test.go
+++ iniconfig/ini_config_test.go
@@ -6,11 +6,20 @@ import (
 )
 
 type Config struct {
+	MysqlConf  MysqlConfig  `ini:"mysql"`
+	ServerConf ServerConfig `ini:"server"`
 }
 type MysqlConfig struct {
+	UserName string `ini:"username"`
+	Passwd   string `ini:"passwd"`
+	Database string `ini:"database"`
+	Host     string `ini:"host"`
+	Port     int    `ini:"port"`
 }
 
 type ServerConfig struct {
+	Ip   string `ini:"ip"`
+	Port int    `ini:"port"`
 }
 
 func TestIniConfig(t *testing.T) {
@@ -18,9 +27,11 @@ func TestIniConfig(t *testing.T) {
 	if err != nil {
 		t.Errorf("read file failed, err:%s", err)
 	}
-	err = UnMarshal(data, &Config{})
+	var conf = &Config{}
+	err = UnMarshal(data, conf)
 	if err != nil {
 		t.Errorf("UnMarshal failed, err:%s", err)
 		return
 	}
+	// fmt.Println(*conf)
 }
